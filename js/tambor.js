bcf1aa0eeed55c9d2c4e268d339ddb81aa4e6e19
juan963303 <estudiantejuandiego963303944>
2024-11-12 10:10:19 +0100

    hola

--- js/tambor.js
+++ js/tambor.js
@@ -1,62 +1,25 @@
+
 // Obtener elementos
 const modal1 = document.getElementById("modal1");
 const openModalBtn1 = document.getElementById("openModal1");
 const closeModalBtn1 = document.querySelector(".close-btn1");
 
-// Función para abrir el modal
+// Función para abrir el modal:3
 openModalBtn1.addEventListener("click", () => {
   modal1.style.display = "flex";
 });
 
-// Función para cerrar el modal
+// Función para cerrar el modal//
 closeModalBtn1.addEventListener("click", () => {
   modal1.style.display = "none";
 });
 
-// Cerrar modal al hacer clic fuera del contenido
+// Cerrar modal al hacer clic fuera del contenido//
 window.addEventListener("click", (event) => {
   if (event.target === modal1) {
     modal1.style.display = "none";
   }
 });
-
-
-
-
-
-
-
-const drums = document.querySelectorAll('.drum');
-
-
-const keyboardkeys = ["v","n","b","m","g","h","j","k","f"];
-
-
-function play(drum) {
-  const audio = document.getElementById(drum.dataset.note);
-  audio.currentTime = 0; // repetir el sodino
-  audio.play(); // Reproducir el sonido
-}
-
-for(let i = 0; i < drums.length; i++){
-
-    drums[i].addEventListener("touchstart", () => play(drums[i]));
-
-}
-
-
-
-document.addEventListener('keydown', e => {
-  if(e.repeat) return;
-
-  const key = e.key.toLowerCase();
-  
-  const keyIndex = keyboardkeys.indexOf(key);
-
-  if(keyIndex > -1) play(drums[keyIndex]);
-
-});
-
 
 
 
@@ -68,9 +31,8 @@ function activateDrum(drum) {
 
     const audio = document.getElementById(drum.dataset.note);
 
-    audio.CurrentTime = 0;
-
-    audio.play();
+    audio.currentTime = 0; // repetir el sodino
+    audio.play(); // Reproducir el sonido
 }
 
 //  desactivar el tambor
@@ -90,17 +52,16 @@ drums2.forEach(drum => {
 document.addEventListener('keydown', (event) => {
     const key = event.key.toLowerCase(); //tecla presionada
 
-    
     const keyToDrumMap = {
-        'n': document.querySelector('.kick-r'), 
-        'v': document.querySelector('.kick'), 
-        'b': document.querySelector('.snare'), 
-        'g': document.querySelector('.tom1'),  
-        'h': document.querySelector('.tom2'),   
-        'j': document.querySelector('.tom3'),   
-        'm': document.querySelector('.hithat'),  
-        'k': document.querySelector('.crash1'), 
-        'f': document.querySelector('.crash2')  
+        'r': document.querySelector('.kick-r'), 
+        'w': document.querySelector('.kick'), 
+        'e': document.querySelector('.snare'), 
+        'y': document.querySelector('.tom1'),  
+        'u': document.querySelector('.tom2'),   
+        'i': document.querySelector('.tom3'),   
+        't': document.querySelector('.hithat'),  
+        'o': document.querySelector('.crash1'), 
+        'q': document.querySelector('.crash2')  
     };
 
     const drum = keyToDrumMap[key];
